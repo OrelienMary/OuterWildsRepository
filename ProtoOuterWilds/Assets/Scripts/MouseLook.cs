f4d2cc2333965deb32bd043079eeca7535d0d4a9
Aurélien <orelien.mary@gmail.com>
2021-02-19 09:24:56 +0100

    ResetupProject

---
+++ ProtoOuterWilds/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+
+    public float mouseSensitivity;
+
+    public Transform pCamera;
+    float cameraRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        cameraRotation -= mouseY;
+        cameraRotation = Mathf.Clamp(cameraRotation, -80f, 80f);
+
+        pCamera.localRotation = Quaternion.Euler(cameraRotation, 0f, 0f);
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ ProtoOuterWilds/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float x;
+    float z;
+
+    public Rigidbody rb;
+
+    public float speed;
+
+    public float gravityValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Vector3 direction = (transform.right * x + transform.forward * z).normalized * speed;
+
+        rb.AddForce(direction * speed * Time.fixedDeltaTime,ForceMode.Force);
+
+        rb.AddForce(-Vector3.up * gravityValue * Time.fixedDeltaTime, ForceMode.Force);
+    }
+}
